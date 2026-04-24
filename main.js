3b5c46c214d5bdc8117828144221e43c669c0770
Bryan Moh <bmoh@A06441.local>
2026-04-24 11:33:51 +1000

    date and time separated

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function updateTime() {
     const now = new Date();
-    const aestTime = now.toLocaleString('en-AU', { timeZone: 'Australia/Sydney' });
-    document.getElementById('timer').innerHTML = aestTime;
+    const aestTime = now.toLocaleString('en-AU', { timeZone: 'Australia/Sydney', hour12: false });
+    const aestDate = now.toLocaleDateString('en-AU', { timeZone: 'Australia/Sydney' });
+    document.getElementById('time').innerHTML = aestTime;
+    document.getElementById('date').innerHTML = aestDate;
 }
 
 setInterval(updateTime, 1000);
