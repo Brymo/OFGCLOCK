96e9bcdfea2b607e95f36bfe3923a37b4eb09089
Bryan Moh <bmoh@A06441.local>
2026-04-24 12:24:07 +1000

    draft

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function updateTime() {
     const now = new Date();
-    const aestTime = now.toLocaleString('en-AU', { timeZone: 'Australia/Sydney', hour12: false });
-    const aestDate = now.toLocaleDateString('en-AU', { timeZone: 'Australia/Sydney' });
+    const aestTime = now.toLocaleTimeString('en-AU', { timeZone: 'Australia/Sydney', hour12: true });
+    const aestDate = now.toLocaleDateString('en-AU', { timeZone: 'Australia/Sydney', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
     document.getElementById('time').innerHTML = aestTime;
     document.getElementById('date').innerHTML = aestDate;
 }
