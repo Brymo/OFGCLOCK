dd41e25e384630868382a9dea91170d3040a1647
Bryan Moh <bmoh@A06441.local>
2026-05-27 09:48:07 +1000

    Added dark mode on page click

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ function updateTime() {
     const aestDate = now.toLocaleDateString('en-AU', { timeZone: 'Australia/Sydney', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
     document.getElementById('time').innerHTML = aestTime;
     document.getElementById('date').innerHTML = aestDate;
+
 }
 
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+
+document.body.addEventListener('click', toggleDarkMode);
 setInterval(updateTime, 1000);
 updateTime();
